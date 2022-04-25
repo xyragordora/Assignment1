87fdbe23138a9ff2940fe2123fb8b5b6f5f0e830
unknown <xgordora@gmail.com>
2022-04-24 16:53:32 -1100

    Message Boxes show the introduction and the full name of the program creator

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace GordoraAssignment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi, this is my first assignment");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I am Xyra Shenne A. Gordora");
+        }
+    }
+}
